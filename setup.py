7020053e2c64561fd0acda9439250ed2b114d065
Gabriel Venturini <dev.venturini@gmail.com>
2024-10-13 04:58:03 -0300

    setup: packages

---
+++ setup.py
@@ -0,0 +1,7 @@
+# this file setup the packages of the app
+
+from setuptools import setup, find_packages
+
+setup(name='BasicInterpreter', version='1.0', packages=find_packages())
+
+# run this on terminal: pip install -e .
